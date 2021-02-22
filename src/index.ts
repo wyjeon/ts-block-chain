6a8aae02edb30d47d875921fd7af0291d52fb4c1
wyjeon <wyjeon.info@gmail.com>
2021-02-22 17:44:11 +0900

    #0.8 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const jeon = new Human('wyjeon', 12, 'male');
+const genesisBlock: Block = new Block(0, '5465421414', '', 'Hello', 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name} you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockChain: [Block] = [genesisBlock];
 
-console.log(sayHi(jeon));
+console.log(blockChain);
 
 export {};
