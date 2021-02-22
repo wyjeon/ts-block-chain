e5a11c03e39012b6502519aa741799a135463d49
wyjeon <wyjeon.info@gmail.com>
2021-02-22 16:39:52 +0900

    #0.5 Types in Typescript

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = 'wyjeon', age = 3, gender = 'male';
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name} you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
