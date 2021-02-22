b9f19a88bf3b276c60dc87e17e74d666eea1d904
wyjeon <wyjeon.info@gmail.com>
2021-02-22 16:26:46 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('hello');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'wyjeon', age = 3, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name} you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
