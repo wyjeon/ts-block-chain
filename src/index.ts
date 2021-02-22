d441da9ed40072ee8f50fc6f581882dbf737a809
wyjeon <wyjeon.info@gmail.com>
2021-02-22 17:37:56 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'wyjeon',
-  age: 42,
-  gender: 'male',
-};
+const jeon = new Human('wyjeon', 12, 'male');
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name} you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(jeon));
 
 export {};
